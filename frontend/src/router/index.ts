e25ab76fb1cea37dcf05de1bd44bb18b940b053d
DESKTOP-TO2B1U4\user <ryoryo8haruharu@gmail.com>
2025-06-06 22:52:23 +0900

    不要なファイルを削除

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,13 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
   {
     path: '/login',
     name: 'login',
